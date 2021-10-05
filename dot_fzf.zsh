9a7428dbf4a7cd93441087238d182b0fdb0ae8a0
MuXiu1997 <muxiu1997@gmail.com>
2021-10-06 01:05:48 +0800

    fzf

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -16,52 +16,33 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 # { Default
     export FZF_COMPLETION_TRIGGER='\'
 
-    FZF_DEFAULT_FILE_PREVIEW='--preview "
-    [[ $(file --mime {}) =~ binary ]] && if [[ -d {} ]] ;then lsd -1 --icon never --color always {}; else echo {} is a binary file;fi ||
-    (bat --theme=Nord --style=numbers --color=always {} ) 2> /dev/null | head -500
-    "'
-    FZF_DEFAULT_COLOR='
+    export FZF_DEFAULT_COLOR='
     --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
     --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
     '
 
-    export FZF_DEFAULT_COMMAND='fd --color=always --follow --exclude .git --exclude node_modules'
-
     export FZF_DEFAULT_OPTS="
     ${FZF_DEFAULT_COLOR}
     "
+
+    ____fzf_funtions="cat ~/.fzf.zsh | grep -E '^####' | cut -c5-"
 # }
 
 # { CTRL-T
     temp_fzf_ctrl_t_type_file="$(mktemp -t fzf_ctrl_t_type.XXXXXXXXXX)"
     temp_fzf_ctrl_t_hidden_file="$(mktemp -t fzf_ctrl_t_hidden.XXXXXXXXXX)"
-    fzf_ctrl_t_files_command="echo \"f\" > ${temp_fzf_ctrl_t_type_file}"
-    fzf_ctrl_t_directories_command="echo \"d\" > ${temp_fzf_ctrl_t_type_file}"
-    fzf_ctrl_t_init_hidden_command="echo \"\" > ${temp_fzf_ctrl_t_hidden_file}"
-    fzf_ctrl_t_toggle_hidden_command="if [[ \$(cat -- \"${temp_fzf_ctrl_t_hidden_file}\") == \"\" ]];then echo \"h\" > ${temp_fzf_ctrl_t_hidden_file};else echo \"\" > ${temp_fzf_ctrl_t_hidden_file};fi"
-    fzf_ctrl_t_base_command="
-        local args=(\"-H\" \"--type\" \"f\" \"--color=always\" \"--follow\" \"--exclude\" \".git\" \"--exclude\" \"node_modules\" \"--exclude\" \"Library\" \"--exclude\" \"Applications\" \"--exclude\" \"System\" \"--exclude\" \"*备份\"); \
-        if [[ \$(cat -- \"${temp_fzf_ctrl_t_hidden_file}\") == \"\" ]]; \
-        then args[1]=\"\"; \
-        else args[1]=\"-H\"; \
-        fi; \
-        if [[ \$(cat -- \"${temp_fzf_ctrl_t_type_file}\") == \"d\" ]]; \
-        then args[3]=\"d\"; \
-        else args[3]=\"f\"; \
-        fi; \
-        fd \${args}
-    "
-    export FZF_CTRL_T_COMMAND="${fzf_ctrl_t_files_command};${fzf_ctrl_t_init_hidden_command};${fzf_ctrl_t_base_command}"
+
+    export FZF_CTRL_T_COMMAND="$____fzf_funtions | zsh -s _fzf_ctrl_t_command 0 \"$temp_fzf_ctrl_t_type_file\" \"$temp_fzf_ctrl_t_hidden_file\""
     export FZF_CTRL_T_OPTS="
     --height '80%'
     --border
     --ansi
     --prompt 'Files> '
     --header 'CTRL-D: Directories / CTRL-F: Files / CTRL-SPACE: Toggle Hidden'
-    --bind 'ctrl-f:change-prompt(Files> )+reload#${fzf_ctrl_t_files_command};${fzf_ctrl_t_base_command}#'
-    --bind 'ctrl-d:change-prompt(Directories> )+reload#${fzf_ctrl_t_directories_command};${fzf_ctrl_t_base_command}#'
-    --bind 'ctrl-space:+reload#${fzf_ctrl_t_toggle_hidden_command};${fzf_ctrl_t_base_command}#'
-    ${FZF_DEFAULT_FILE_PREVIEW}
+    --bind 'ctrl-f:change-prompt(Files> )+reload($____fzf_funtions | zsh -s _fzf_ctrl_t_command 1 \"$temp_fzf_ctrl_t_type_file\" \"$temp_fzf_ctrl_t_hidden_file\")'
+    --bind 'ctrl-d:change-prompt(Directories> )+reload($____fzf_funtions | zsh -s _fzf_ctrl_t_command 2 \"$temp_fzf_ctrl_t_type_file\" \"$temp_fzf_ctrl_t_hidden_file\")'
+    --bind 'ctrl-space:+reload($____fzf_funtions | zsh -s _fzf_ctrl_t_command 3 \"$temp_fzf_ctrl_t_type_file\" \"$temp_fzf_ctrl_t_hidden_file\")'
+    --preview '$____fzf_funtions | sh -s _file_preview {}'
     "
 # }
 
@@ -76,3 +57,56 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
     --bind 'ctrl-space:toggle-preview'
     "
 # }
+
+
+####set -e
+####
+####_file_preview() {
+####    filepath="$1"
+####    if [[ $(file --mime "$filepath") =~ binary ]]; then
+####        if [[ -d "$filepath" ]]; then
+####            exec lsd -1 --icon=never --color=always "$filepath"
+####        else
+####            echo ""$filepath" is a binary file"
+####        fi
+####    else
+####        exec bat --theme=Nord --style=numbers --color=always "$filepath" 2> /dev/null | head -500
+####    fi
+####}
+####
+####_fzf_ctrl_t_command() {
+####    temp_type_file="$2"
+####    temp_hidden_file="$3"
+####
+####    case $1 in
+####        0) 
+####            echo "f" > $temp_type_file
+####            echo "" > $temp_hidden_file
+####        ;;
+####        1) 
+####            echo "f" > $temp_type_file
+####        ;;
+####        2) 
+####            echo "d" > $temp_type_file
+####        ;;
+####        3)
+####            if [[ "$(cat -- $temp_hidden_file)" == "" ]]; then
+####                echo "h" > $temp_hidden_file
+####            else 
+####                echo "" > $temp_hidden_file
+####            fi
+####        ;;
+####    esac
+####
+####    fd_args=("-H" "--type" "f" "--color=always" "--follow" "--exclude" ".git" "--exclude" "node_modules" "--exclude" "Library" "--exclude" "Applications" "--exclude" "System" "--exclude" "*备份")
+####    if [[ "$(cat -- $temp_hidden_file)" == "" ]]; then
+####        fd_args[1]=""
+####    fi
+####    if [[ "$(cat -- $temp_type_file)" == "d" ]]; then
+####        fd_args[3]="d"
+####    fi
+####    exec fd $fd_args
+####}
+####
+####"$@"
+
