e7b54ee2401967b713fef32984a6e123860132d1
MuXiu1997 <muxiu1997@gmail.com>
2021-11-13 10:52:12 +0800

    fzf-tab

--- dot_config/fzf-tab.zsh
+++ dot_config/fzf-tab.zsh
@@ -1,3 +1,16 @@
 enable-fzf-tab
 zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
 
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ':fzf-tab:*' show-group full
+zstyle ':fzf-tab:*' single-group color header
+zstyle ':fzf-tab:*' switch-group ',' '.'
+zstyle ':fzf-tab:*' prefix ''
+
+zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' \
+    fzf-preview 'echo ${(P)word}'
+zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' \
+    popup-pad 100 5
+zstyle ':fzf-tab:complete:(-command-|-parameter-|-brace-parameter-|export|unset|expand):*' \
+    fzf-flags --preview-window=down:3:wrap
+
