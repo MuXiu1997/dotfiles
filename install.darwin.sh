f0aed25678352fc876916b9bf51d3b62239b0325
MuXiu1997 <muxiu1997@gmail.com>
2021-11-21 14:48:36 +0800

    chezmoi

--- install.darwin.sh
+++ install.darwin.sh
@@ -5,22 +5,25 @@ set -e
 tmpdir=$(mktemp -d)
 trap 'rm -rf ${tmpdir}' EXIT
 
+# ensure chezmoi
 if [ ! "$(command -v chezmoi)" ]; then
     sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$tmpdir"
 fi
 
+# ensure bitwarden-cli
 if [ ! "$(command -v bw)" ]; then
     bw="$tmpdir/bw"
-    bw_version=$(curl -fsSL -o /dev/null -w "%{url_effective}" "https://github.com/bitwarden/cli/releases/latest" | awk -v FS='/' '{print $NF}' | cut -c 2-)
-    curl -w '%{http_code}' -sL -o  "$tmpdir/bw-macos-$bw_version.zip" "https://github.com/bitwarden/cli/releases/download/v$bw_version/bw-macos-$bw_version.zip"
-    unzip "$tmpdir/bw-macos-$bw_version.zip"
+    curl -sL -o "$tmpdir/bw.zip" 'https://vault.bitwarden.com/download/?app=cli&platform=macos'
+    unzip "$tmpdir/bw.zip" -d "$tmpdir"
     chmod +x "$bw"
 else
     bw=bw
 fi
 
+# config bitwarden and login
 $bw config server https://bw.muxiu1997.com
 $bw logout || true
 BW_SESSION=$($bw login --raw)
 
-sh -c "PATH=$tmpdir:$PATH BW_SESSION=$BW_SESSION chezmoi init --apply -v MuXiu1997"
+# chezmoi init
+sh -c "PATH=$tmpdir:$PATH BW_SESSION=$BW_SESSION chezmoi init --apply --verbose MuXiu1997"
