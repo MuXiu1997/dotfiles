46fc76e6b608efd22d16aad8505faa74f806d3f1
MuXiu1997 <muxiu1997@gmail.com>
2022-08-22 00:39:39 +0800

    chezmoi

--- .chezmoiscripts/run_once_after_98-ensure-GOPATH.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eufo pipefail
-
-XDG_DATA_HOME="$HOME/.local/share"
-GOPATH="$XDG_DATA_HOME/go"
-mkdir -p "$GOPATH/src" "$GOPATH/bin" "$GOPATH/pkg"
--- .chezmoiscripts/run_once_before_01-XDG-hammerspoon.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -eufo pipefail
-
-defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
