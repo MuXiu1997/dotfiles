967e79ca035ef7d38e5155e4fcff0f4754b918d5
MuXiu1997 <muxiu1997@gmail.com>
2023-02-21 10:52:43 +0800

    Update lazygit custom commands

--- dot_config/lazygit/scripts/__global.py
+++ dot_config/lazygit/scripts/__global.py
@@ -1,4 +1,9 @@
 import os
+import subprocess
+# noinspection PyUnresolvedReferences
+import sys
+# noinspection PyUnresolvedReferences
+import time
 
 os.environ['GUM_CHOOSE_CURSOR_FOREGROUND'] = '6'
 os.environ['GUM_CHOOSE_SELECTED_FOREGROUND'] = '6'
@@ -11,3 +16,51 @@ os.environ['GUM_INPUT_WIDTH'] = '100'
 os.environ['GUM_SPIN_SPINNER_FOREGROUND'] = '6'
 os.environ['GUM_WRITE_CURSOR_FOREGROUND'] = '6'
 os.environ['GUM_WRITE_PROMPT_FOREGROUND'] = '4'
+
+
+def get_remotes():
+    cmd = subprocess.Popen(
+        ['git', 'remote'],
+        stdout=subprocess.PIPE,
+    )
+    stdout, _ = cmd.communicate()
+    remotes = stdout.decode().splitlines()
+    if 'origin' in remotes:
+        remotes = ['origin'] + [remote for remote in remotes if remote != 'origin']
+    return remotes
+
+
+def get_local_branches():
+    cmd = subprocess.Popen(
+        ['git', 'branch', '--format=%(refname:short)'],
+        stdout=subprocess.PIPE,
+    )
+    stdout, _ = cmd.communicate()
+    return stdout.decode().splitlines()
+
+
+def get_remote_branches():
+    cmd = subprocess.Popen(
+        ['git', 'branch', '-r', '--format=%(refname:short)'],
+        stdout=subprocess.PIPE,
+    )
+    stdout, _ = cmd.communicate()
+    return stdout.decode().splitlines()
+
+
+def get_local_branch_commit(branch):
+    cmd = subprocess.Popen(
+        ['git', 'rev-parse', 'refs/heads/{}'.format(branch)],
+        stdout=subprocess.PIPE,
+    )
+    stdout, _ = cmd.communicate()
+    return stdout.decode().strip()
+
+
+def get_head_commit():
+    cmd = subprocess.Popen(
+        ['git', 'rev-parse', 'HEAD'],
+        stdout=subprocess.PIPE,
+    )
+    stdout, _ = cmd.communicate()
+    return stdout.decode().strip()
